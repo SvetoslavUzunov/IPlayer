0add2a972cc34b147c48f4a1d3a40d8bdc1c18a6
Svetoslav Uzunov <svetoslavuzunov99@gmail.com>
2022-08-28 12:55:09 +0300

    Initial create
    Upload files

---
+++ IPlayer/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace IPlayer;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
---
+++ IPlayer/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace IPlayer;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
