dca8a7b48c995d3aab33aa75e986501109cf4404
Svetoslav Uzunov <svetoslavuzunov99@gmail.com>
2022-09-05 17:49:53 +0300

    Work on view controls

--- IPlayer/MauiProgram.cs
+++ IPlayer/MauiProgram.cs
@@ -1,4 +1,8 @@
-﻿namespace IPlayer;
+﻿using Android.App;
+using Android.Views;
+using Microsoft.Maui.LifecycleEvents;
+
+namespace IPlayer;
 
 public static class MauiProgram
 {
@@ -11,6 +15,19 @@ public static class MauiProgram
 			{
 				fonts.AddFont("FiraSans-Light.ttf", "RegularFont");
 				fonts.AddFont("FiraSans-Medium.ttf", "MediumFont");
+			})
+			.ConfigureLifecycleEvents(events =>
+			{
+#if ANDROID
+				events.AddAndroid(android => android.OnCreate((activity, bundle) => MakeStatusBarTranslucent(activity)));
+
+				static void MakeStatusBarTranslucent(Activity activity)
+				{
+					activity.Window.SetFlags(WindowManagerFlags.LayoutNoLimits, WindowManagerFlags.LayoutNoLimits);
+					activity.Window.ClearFlags(WindowManagerFlags.TranslucentStatus);
+					activity.Window.SetStatusBarColor(Android.Graphics.Color.Transparent);
+				}
+#endif
 			});
 
 		return builder.Build();
---
+++ IPlayer/ViewModels/Base/AppViewModelBase.cs
@@ -0,0 +1,25 @@
+﻿using CommunityToolkit.Mvvm.Input;
+using IPlayer.IServices;
+using Maui.Apps.Framework.MVVM;
+
+namespace IPlayer.ViewModels.Base;
+
+public partial class AppViewModelBase : BaseViewModel
+{
+	public AppViewModelBase(IApiService apiService) : base()
+		=> this.apiService = apiService;
+
+	public INavigation NavigationService { get; set; }
+
+	public Page PageService { get; set; }
+
+	protected IApiService apiService { get; set; }
+
+	[RelayCommand]
+	private async Task NavigationBack()
+		=> await NavigationService.PopAsync();
+
+	[RelayCommand]
+	private async Task CloseModal()
+		=> await NavigationService.PopModalAsync();
+}
